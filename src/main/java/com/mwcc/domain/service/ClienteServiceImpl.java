099ed2704229a9518cb80ede5ca03d0d04c451b0
marcio willian chaves cardoso <willian_mw.cc@hotmail.com>
2022-09-13 18:36:19 -0300

    M - Refatorando ClienteService e ProdutoService para usar interface

---
+++ src/main/java/com/mwcc/domain/service/ClienteServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mwcc.domain.service;
+
+import com.mwcc.domain.repository.Clientes;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@AllArgsConstructor
+public class ClienteServiceImpl implements ClienteService{
+
+    private final Clientes clientes;
+
+    public void deletar (Integer clienteId){
+        clientes.findById(clienteId)
+                .map(cliente -> {
+                    clientes.delete(cliente);
+                    return cliente;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+
+    }
+
+}
