6c37ad9c4260719f3524457fdc601b4c8da92894
Marcio Willian <willian_mw.cc@hotmail.com>
2022-09-24 21:01:03 -0300

    D - Implementando validação no cadastro de Pedido e na atualização de status do Pedido

--- src/main/java/com/mwcc/api/controller/PedidoController.java
+++ src/main/java/com/mwcc/api/controller/PedidoController.java
@@ -14,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
@@ -28,7 +29,7 @@ public class PedidoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Integer save(@RequestBody PedidoDTO dto){
+    public Integer save(@RequestBody @Valid PedidoDTO dto){
         Pedido pedido = pedidoService.salvar(dto);
         return pedido.getId();
     }
@@ -57,7 +58,7 @@ public class PedidoController {
 
     @PatchMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateStatus(@PathVariable Integer id, @RequestBody AtualizacaoStatusPedidoDTO dto){
+    public void updateStatus(@PathVariable Integer id, @RequestBody @Valid AtualizacaoStatusPedidoDTO dto){
         pedidoService.atualizarStatus(id, StatusPedido.valueOf(dto.getNovoStatus()));
     }
 
--- src/main/java/com/mwcc/domain/dto/AtualizacaoStatusPedidoDTO.java
+++ src/main/java/com/mwcc/domain/dto/AtualizacaoStatusPedidoDTO.java
@@ -3,8 +3,11 @@ package com.mwcc.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 public class AtualizacaoStatusPedidoDTO {
+    @NotEmpty(message = "O campo status é obrigatório." )
     private String novoStatus;
 }
