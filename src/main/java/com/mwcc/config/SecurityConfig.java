43b18ac0611da71b4c37c6a5ec828289d7d9d97a
Marcio Willian <willian_mw.cc@hotmail.com>
2022-09-25 20:45:42 -0300

    D - Configurando perfís de usuarios em memória

--- src/main/java/com/mwcc/config/SecurityConfig.java
+++ src/main/java/com/mwcc/config/SecurityConfig.java
@@ -28,6 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/clientes/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/pedidos")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api.produtos/**")
+                        .hasRole("ADMIN")
+                .and()
+                    .formLogin();
     }
 }
